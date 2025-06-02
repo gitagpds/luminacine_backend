278f89ea7afcd4ce94429eef3449858abf2c6a03
Laras Ayo <149316954+Ayazzzzzzz@users.noreply.github.com>
2025-06-03 01:23:42 +0700

    coba coba brot brot

--- index.js
+++ index.js
@@ -17,10 +17,17 @@ app.use(cookieParser());
 // CORS Configuration - UBAH BAGIAN INI
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",                           // untuk development
-      "https://g-07-450802.uc.r.appspot.com"           // untuk production
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://g-07-450802.uc.r.appspot.com"
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
