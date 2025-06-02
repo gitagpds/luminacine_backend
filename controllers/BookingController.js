0c7a65c4f52e8d821224899e7faf38f940d8f4fc
Gita Poetri Dewi Siregar <146951904+gitagpds@users.noreply.github.com>
2025-06-02 22:59:55 +0700

    Update BookingController.js

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -3,7 +3,7 @@ import Seat from "../models/SeatModel.js";
 import Schedule from "../models/ScheduleModel.js";
 import Movie from "../models/MovieModel.js";
 import User from "../models/UserModel.js";
-const BookingSeats = require('../models/BookingSeats');
+import BookingSeats from "../models/BookingSeatsModel.js";
 
 // GET ALL BOOKINGS
 async function getBookings(req, res) {
